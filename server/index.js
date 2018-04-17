d862aa44838fbefec1766242bbe0220c324781ae
Nazarov MI <nazarov-mi@mail.ru>
2018-04-17 05:39:28 +0300

    Добавит скрипты сервера

---
+++ server/index.js
@@ -0,0 +1,119 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const favicon = require('serve-favicon');
+const morgan = require('morgan');
+const serveStatic = require('serve-static');
+const cookieParser = require('cookie-parser');
+const expressSession = require('express-session');
+const slashes = require('connect-slashes');
+const passport = require('passport');
+// LocalStrategy = require('passport-local').Strategy,
+const csrf = require('csurf');
+const compression = require('compression');
+const useragent = require('express-useragent');
+
+const config = require('./config');
+const staticFolder = path.resolve(__dirname, '..', config.staticFolder);
+
+const Render = require('./render');
+const render = Render.render;
+const dropCache = Render.dropCache; // eslint-disable-line no-unused-vars
+
+const port = process.env.PORT || config.defaultPort;
+const isSocket = isNaN(port);
+const isDev = process.env.NODE_ENV === 'development';
+
+const articles = require('./../src/data.js')
+
+require('debug-http')();
+
+app
+	.disable('x-powered-by')
+	.enable('trust proxy')
+	.use(compression())
+	.use(favicon(path.join(staticFolder, 'favicon.png')))
+	.use(serveStatic(staticFolder))
+	.use(morgan('combined'))
+	.use(cookieParser())
+	.use(bodyParser.urlencoded({ extended: true }))
+	.use(expressSession({
+		resave: true,
+		saveUninitialized: true,
+		secret: config.sessionSecret
+	}))
+	.use(passport.initialize())
+	.use(passport.session())
+	.use(csrf());
+
+// NOTE: conflicts with livereload
+isDev || app.use(slashes());
+
+passport.serializeUser(function(user, done) {
+	done(null, JSON.stringify(user));
+});
+
+passport.deserializeUser(function(user, done) {
+	done(null, JSON.parse(user));
+});
+
+app.use('/', function(req, res, next) {
+	const uaHeader = req.headers['user-agent'];
+	const ua = useragent.parse(uaHeader);
+	let platformName = 'desktop'
+
+	if (ua.isTablet) {
+		platformName = 'tablet';
+	} else
+	if (ua.isMobile) {
+		platformName = 'mobile';
+	}
+
+	req.platformName = platformName;
+
+	next();
+});
+
+app.get('/ping/', function(req, res) {
+	res.send('ok');
+});
+
+app.get('/', function(req, res) {
+	render(req, res, {
+		articles,
+		platformName: req.platformName,
+		view: 'page-index',
+		title: 'DZEN',
+		meta: {
+			description: 'shri-homework-12',
+			og: {
+				url: 'https://example.loc',
+				siteName: 'shri-homework-12'
+			}
+		}
+	})
+});
+
+isDev && require('./rebuild')(app);
+
+app.get('*', function(req, res) {
+	res.status(404);
+	return render(req, res, { view: '404' });
+});
+
+if (isDev) {
+	app.get('/error/', function() {
+		throw new Error('Uncaught exception from /error');
+	});
+
+	app.use(require('errorhandler')());
+}
+
+isSocket && fs.existsSync(port) && fs.unlinkSync(port);
+
+app.listen(port, function() {
+	isSocket && fs.chmod(port, '0777');
+	console.log('server is listening on', this.address().port);
+});
---
+++ server/render.js
@@ -0,0 +1,96 @@
+const fs = require('fs');
+const path = require('path');
+const nodeEval = require('node-eval');
+const config = require('./config');
+
+const isDev = process.env.NODE_ENV === 'development';
+const useCache = !isDev;
+const cacheTTL = config.cacheTTL;
+const templatesCache = Object.create(null)
+let cache = Object.create(null);
+
+function render(req, res, data, context) {
+	const query = req.query;
+	const user = req.user;
+	const cacheKey = req.originalUrl + (context ? JSON.stringify(context) : '') + (user ? JSON.stringify(user) : '');
+	const cached = cache[cacheKey];
+
+	if (useCache && cached && (new Date() - cached.timestamp < cacheTTL)) {
+		return res.send(cached.html);
+	}
+
+	if (isDev && query.json) return res.send('<pre>' + JSON.stringify(data, null, 4) + '</pre>');
+
+	const bemtreeCtx = {
+		block: 'root',
+		context: context,
+		// extend with data needed for all routes
+		data: Object.assign({}, {
+			url: req._parsedUrl,
+			csrf: req.csrfToken()
+		}, data)
+	};
+
+	const platformName = req.platformName;
+	const templates = getTemplates(platformName, platformName);
+
+	let bemjson;
+
+	try {
+		bemjson = templates.BEMTREE.apply(bemtreeCtx);
+	} catch(err) {
+		console.error('BEMTREE error', err.stack);
+		console.trace('server stack');
+		return res.sendStatus(500);
+	}
+
+	if (isDev && query.bemjson) return res.send('<pre>' + JSON.stringify(bemjson, null, 4) + '</pre>');
+
+	let html;
+
+	try {
+		html = templates.BEMHTML.apply(bemjson);
+	} catch(err) {
+		console.error('BEMHTML error', err.stack);
+		return res.sendStatus(500);
+	}
+
+	useCache && (cache[cacheKey] = {
+		timestamp: new Date(),
+		html: html
+	});
+
+	res.send(html);
+}
+
+function dropCache() {
+	cache = Object.create(null);
+}
+
+function evalFile(filename) {
+	return nodeEval(fs.readFileSync(filename, 'utf8'), filename);
+}
+
+function createTemplates(platformName, bundleName) {
+	const pathToBundle = path.resolve(__dirname, '..', `${platformName}.bundles`, bundleName);
+
+	return {
+		BEMTREE: evalFile(path.join(pathToBundle, `${bundleName}.bemtree.js`)).BEMTREE,
+		BEMHTML: evalFile(path.join(pathToBundle, `${bundleName}.bemhtml.js`)).BEMHTML
+	};
+}
+
+function getTemplates(platformName = 'desktop', bundleName = 'index') {
+	const key = `${platformName}_${bundleName}`;
+
+	if (templatesCache[key] && !isDev) {
+		return templatesCache[key];
+	}
+
+	return (templatesCache[key] = createTemplates(platformName, bundleName));
+}
+
+module.exports = {
+	render: render,
+	dropCache: dropCache
+};
